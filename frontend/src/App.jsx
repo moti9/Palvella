ed8e06ebe6c6843bd722e518d1c01d1c2d767abe
Pankajkantghz <118116376+Pankajkantghz@users.noreply.github.com>
2024-03-18 10:16:31 +0530

    Create App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+
+
+import Landing from "./Pages/Landing";
+import Shop from "./Pages/Shop";
+import Cart from "./Pages/Cart";
+const App=()=>(
+    <>
+    
+    <Landing />
+     <Shop />
+     <Cart />
+     
+      
+    </>
+  );
+
+
+export default App;
