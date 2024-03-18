3779aa8a5aa85805d0d8583ebd66d9d64f5f89ed
Pankajkantghz <118116376+Pankajkantghz@users.noreply.github.com>
2024-03-18 10:22:58 +0530

    Create Shop.jsx

---
+++ frontend/src/pages/Shop.jsx
@@ -0,0 +1,15 @@
+import AboutShop from "../components/Aboutshop";
+import Nav from "../components/Nav";
+import Menu from "../components/Menu";
+import Mainfooter from "../components/Mainfooter";
+const Shop = () =>(
+    <>
+    <Nav />
+    <AboutShop />
+    <Menu />
+    <Mainfooter />
+    </>
+
+  );
+
+export default Shop;
